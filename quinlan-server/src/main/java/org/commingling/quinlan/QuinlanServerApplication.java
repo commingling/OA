7ca2d72e33a4d06d16a3b287e86233cae18d8ddd
liubaoming <quinlanliuming@163.com>
2023-02-28 15:58:47 +0800

    feat：关闭  spring security 登录窗口 =========================================================== feat：提交新功能 fix：修复了bug docs：只修改了文档 style：调整代码格式，未修改代码逻辑（比如修改空格、格式化、缺少分号等） refactor：代码重构，既没修复bug也没有添加新功能 perf：性能优化，提高性能的代码更改 test：添加或修改代码测试 chore：对构建流程或辅助工具和依赖库（如文档生成等）的更改

--- quinlan-server/src/main/java/org/commingling/quinlan/QuinlanServerApplication.java
+++ quinlan-server/src/main/java/org/commingling/quinlan/QuinlanServerApplication.java
@@ -1,7 +1,9 @@
 package org.commingling.quinlan;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -10,7 +12,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
  * @author ：Quinlan
  * @date ：2022/11/07 14:55
  */
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class, ManagementWebSecurityAutoConfiguration.class})
 public class QuinlanServerApplication {
 
     public static void main(String[] args) {
