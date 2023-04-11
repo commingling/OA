199afbc8c019fd538be3d711447e0dcec994ebcf
commingling <quinlanliuming@163.com>
2023-04-11 15:55:30 +0800

    delete：删除重来 ============================================================ feat：提交新功能 fix：修复了bug docs：只修改了文档 style：调整代码格式，未修改代码逻辑（比如修改空格、格式化、缺少分号等） refactor：代码重构，既没修复bug也没有添加新功能 perf：性能优化，提高性能的代码更改 test：添加或修改代码测试 chore：对构建流程或辅助工具和依赖库（如文档生成等）的更改

--- quinlan-server/src/main/java/org/commingling/quinlan/QuinlanServerApplication.java
+++ quinlan-server/src/main/java/org/commingling/quinlan/QuinlanServerApplication.java
@@ -1,27 +1,13 @@
 package org.commingling.quinlan;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
-/**
- * @author ：Quinlan
- * @date ：2022/11/07 14:55
- */
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class, ManagementWebSecurityAutoConfiguration.class})
+@SpringBootApplication
 public class QuinlanServerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(QuinlanServerApplication.class, args);
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
     }
 
 }
